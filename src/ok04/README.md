a26227ed4542e3677d54f1a7a6ca3aaf35d9a29f
Mauri de Souza Nunes <mauri870@gmail.com>
2017-12-14 13:54:18 -0200

    Add ok04 exercise

---
+++ src/ok04/README.md
@@ -0,0 +1,7 @@
+# OK04 -  How to use the timer to flash the 'OK' or 'ACT' LED at precise intervals
+
+The OK04 lesson builds on OK03 by teaching how to use the timer to flash the 'OK' or 'ACT' LED at precise intervals.
+
+## References
+
+http://www.studica.com/blog/raspberry-pi-timer-embedded-environments
---
+++ src/ok04/main.s
@@ -0,0 +1,28 @@
+@;
+@;  A simple program to blink the OK/ACT LED on Raspberry Pi 3
+@;
+
+.section .init                      @; kernel initialization code must be on 0x8000
+.global _start                      @; define _start label globally available for the linker
+_start:
+    mov         sp, #0x8000         @; set up the stack pointer
+    b           _main               @; branch to main routine
+
+.section .text
+_main:
+    loop$:                          @; main loop
+        ldr     r0, =0xf4240        @; delay in microseconds (1s)
+        bl      delay               @; branch to delay function
+
+        @; enable led
+        mov     r0, #1              @; led state 1 = on
+        bl      set_led_state       @; set led state
+
+        ldr     r0, =0xf4240        @; delay in microseconds (1s)
+        bl      delay               @; branch to delay function
+
+        @; disable led
+        mov     r0, #0              @; led state 0 = off
+        bl      set_led_state       @; set led state
+
+        b       loop$               @; branch to main loop$

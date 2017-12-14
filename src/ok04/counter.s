00304e296de3eafe441e42cb32820798412ad3bb
Mauri de Souza Nunes <mauri870@gmail.com>
2017-12-14 17:22:29 -0200

    Fix typo

--- src/ok04/counter.s
+++ src/ok04/counter.s
@@ -6,7 +6,7 @@
 .global delay
 delay:
     ldr         r1, =0x3F000000     @; pi peripheral address
-    orr         r1, r1, #0x3000     @; r6 = r1 | 0x3000 = 0x3F003000 timer base address
+    orr         r1, r1, #0x3000     @; r1 = r1 | 0x3000 = 0x3F003000 timer base address
     ldr         r2, [r1, #0x4]      @; time in microseconds given by the timer
     delay1$:
         ldr     r3, [r1, #0x4]      @; get timer in microseconds in each iteration

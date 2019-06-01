4f90fcdb0de4fba2cb5540ea948e44b1e9985126
Mauri de Souza Nunes <mauri870@gmail.com>
2019-06-01 19:45:22 -0300

    Fix duplicated code in ok03 mailbox.s

--- src/ok03/mailbox.s
+++ src/ok03/mailbox.s
@@ -35,21 +35,3 @@ mailbox_read:
 
     and         r0, r2, #0xfffffff0             @; move the response (top 28 bits of mail) into r0
     pop         {pc}                            @; return
-    push        {lr}                            @; push the address the function should return to
-    mov         r1, r0                          @; save the channel in r1
-    ldr         r0, =0x3f00b880                 @; load mailbox base address into r0
-
-    right_mail$:
-        wait_read$:
-            ldr     r2, [r0, #0x18]             @; get status of mailbox 0 (VC -> ARM)
-            tst     r2, #0x40000000             @; check if the mailbox is empty
-            bne     wait_read$                  @; if VC FIFO queue is empty branch to wait_read
-
-        ldr     r2, [r0]                        @; load the address of response data
-
-        and     r3, r2, #0b1111                 @; extract the channel, the lowest 4 bits
-        teq     r3, r1                          @; check if the response channel is the same we want
-        bne     right_mail$                     @; if the channel is wrong branch to wait_read
-
-    and         r0, r2, #0xfffffff0             @; move the response (top 28 bits of mail) into r0
-    pop         {pc}                            @; return
